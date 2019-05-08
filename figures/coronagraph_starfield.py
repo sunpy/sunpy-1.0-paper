ac522f28163e0ca02e4a9c7c4448ae7dfac37a1d
Steven Christe <steven.christe@nasa.gov>
2019-05-08 15:17:47 -0400

    fixes

---
+++ figures/coronagraph_starfield.py
@@ -0,0 +1,61 @@
+import os
+import matplotlib.pyplot as plt
+import sunpy.map
+from sunpy.coordinates import get_body_heliographic_stonyhurst, frames
+
+# look up stars in a star catalog
+from astroquery.vizier import Vizier
+from astropy import coordinates
+import astropy.units as u
+from astropy.coordinates import SkyCoord
+
+from sunpy_paper import data_dir
+
+f = '2014_05_15__07_54_00_005__STEREO-A_SECCHI_COR2_white-light.jp2'
+
+path = os.path.join(data_dir, f)
+
+map1 = sunpy.map.Map(path)
+
+# gaia
+vv=Vizier(columns=['**'], row_limit=-1, column_filters={'Gmag':'<7'}, timeout=1200)
+vv.ROW_LIMIT = -1
+result = vv.query_constraints(catalog='I/345/gaia2')
+
+hpc_coords = []
+source = []
+hpc_x = []
+hpc_y = []
+
+for this_object in result[0]:
+    tbl_crds = coordinates.SkyCoord(this_object['RA_ICRS'] * u.deg, this_object['DE_ICRS'] * u.deg, 1e12 * u.km, frame='icrs', obstime=map1.date)
+    source.append(this_object['Source'])
+    this_hpc = tbl_crds.transform_to(frames.Helioprojective(observer=map1.observer_coordinate))
+    hpc_x.append(this_hpc.Tx.value)
+    hpc_y.append(this_hpc.Ty.value)
+    hpc_coords.append(this_hpc)
+
+star_pixels = map1.world_to_pixel(SkyCoord(hpc_x * u.arcsec, hpc_y * u.arcsec, frame=map1.coordinate_frame))
+
+mars = get_body_heliographic_stonyhurst('mars', map1.date, observer=map1.observer_coordinate)
+mars_hpc = mars.transform_to(frames.Helioprojective(observer=map1.observer_coordinate))
+
+fig = plt.figure(figsize=(20,20))
+ax1 = fig.add_subplot(1,1,1, projection=map1)
+
+
+map1.plot(axes=ax1, vmin=0, vmax=400)
+map1.draw_limb()
+#for this_coord in hpc_coords:
+#    ax1.plot_coord(this_coord, 'o', color='red', fillstyle='none')
+ax1.plot(star_pixels.x, star_pixels.y, 'o', fillstyle='none', color='white', markersize=12)
+ax1.plot_coord(mars_hpc, 's', color='white', fillstyle='none', markersize=12, label='Mars')
+
+fov = (4.0 * u.deg).to('arcsec').value
+lims_arcsec = ((-fov, fov)*u.arcsec, (-fov, fov)*u.arcsec)
+lims_pix_ax1 = map1.world_to_pixel(SkyCoord(*lims_arcsec, frame=map1.coordinate_frame))
+ax1.set_xlim(lims_pix_ax1[0].value)
+ax1.set_ylim(lims_pix_ax1[1].value)
+plt.legend()
+
+plt.savefig('coronagraph_starfield.pdf')
