bda701110c39001f5aa77707d32efa9bb4a26805
Steven Christe <steven.christe@nasa.gov>
2019-06-07 16:31:19 -0400

    plot updates

--- figures/loc_vs_time.py
+++ figures/loc_vs_time.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 import sunpy_paper
-sunpy_paper.setup_plot()
 
 datafile_url = os.path.join(sunpy_paper.data_dir, 'loc_vs_time.csv')
 data = pd.read_csv(datafile_url, parse_dates=True, index_col=0, comment='#')
@@ -22,11 +21,11 @@ ax1.plot(x, y2, label='comment', color='black', linestyle='--')
 
 
 plt.ylabel('Lines (thousands)')
-plt.ylim(0, 4)
+plt.ylim(0, 6)
 
 plt.legend(loc=2)
 
 # add vertical lines for the releases
 sunpy_paper.add_releases_vs_time(ax1)
 
-plt.savefig('loc_vs_time.pdf')
+plt.savefig('fig_loc_vs_time.pdf')
