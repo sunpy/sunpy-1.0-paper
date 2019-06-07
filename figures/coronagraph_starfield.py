08e1b70c739fcaaf1c96fd0edd4e1521510367e4
Steven Christe <steven.christe@nasa.gov>
2019-06-07 16:14:30 -0400

    merge fix

--- figures/coronagraph_starfield.py
+++ figures/coronagraph_starfield.py
@@ -51,19 +51,19 @@ result = vv.query_region(search_coord, radius=4 * u.deg, catalog='I/345/gaia2')
 hpc_coords = []
 for this_object in result[0]:
     tbl_crds = SkyCoord(this_object['RA_ICRS'] * u.deg, this_object['DE_ICRS'] * u.deg,
-                        1e12 * u.km, frame='icrs', obstime=map1.date)
+                        1e12 * u.km, frame='icrs', obstime=cor2.date)
     hpc_coords.append(tbl_crds.transform_to(
-        frames.Helioprojective(observer=map1.observer_coordinate)))
+        frames.Helioprojective(observer=cor2.observer_coordinate)))
 
 # get the location of mars
 mars = get_body_heliographic_stonyhurst(
-    'mars', map1.date, observer=map1.observer_coordinate)
+    'mars', cor2.date, observer=cor2.observer_coordinate)
 mars_hpc = mars.transform_to(
-    frames.Helioprojective(observer=map1.observer_coordinate))
+    frames.Helioprojective(observer=cor2.observer_coordinate))
 
 # now plot
 fig = plt.figure(figsize=(5,5))
-ax = fig.add_subplot(111, projection=map1)
+ax = fig.add_subplot(111, projection=cor2)
 
 cor2.plot(axes=ax, vmin=0, vmax=600)
 cor2.draw_limb()
