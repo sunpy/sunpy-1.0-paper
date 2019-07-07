f91e74758e7cae57d951299cc094534ad0764cf6
Laura Hayes <hayesla@tcd.ie>
2019-07-07 19:02:18 -0400

    adding map example code and pdf

---
+++ figures/map_example.py
@@ -0,0 +1,44 @@
+import matplotlib.pyplot as plt
+import sunpy.map
+import sunpy.data.sample
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
+import matplotlib as mpl
+
+testy = {'font.family' : 'serif',
+'font.serif' : 'Computer Modern',
+'text.usetex' : True,
+'axes.titlesize' : 12,
+'axes.labelsize' : 12,
+'legend.fontsize' : 12,
+'xtick.labelsize' : 12,
+'ytick.labelsize' : 12,
+'xtick.major.pad' : 3,
+'xtick.minor.pad' : 3,
+'ytick.major.pad' : 3,
+'ytick.minor.pad' : 3,
+'xtick.direction' : 'in',
+'ytick.direction' : 'in',
+'savefig.dpi' : 200,
+'savefig.format' : 'pdf',
+'savefig.bbox' : 'tight',
+'backend': 'TkAgg'}
+
+for key in testy:
+    mpl.rcParams[key] = testy[key]
+
+my_map = sunpy.map.Map(sunpy.data.sample.AIA_171_IMAGE)
+
+top_right = SkyCoord(1200 * u.arcsec, 0 * u.arcsec, frame=my_map.coordinate_frame)
+bottom_left = SkyCoord(500 * u.arcsec, -700 * u.arcsec, frame=my_map.coordinate_frame)
+my_submap = my_map.submap(bottom_left, top_right)
+
+fig = plt.figure(figsize=(12, 6))
+ax0 = fig.add_subplot(1, 2, 1,  projection=my_map)
+my_map.plot(clip_interval=(1, 99.99)*u.percent, axes=ax0)
+my_map.draw_rectangle(bottom_left, 700*u.arcsec, 700*u.arcsec)
+ax1 = fig.add_subplot(1, 2, 2, projection=my_submap)
+my_submap.plot(clip_interval=(1, 99.7)*u.percent, title='', axes=ax1)
+my_submap.draw_grid(axes=ax1)
+plt.savefig('map_example.pdf', dpi=200)
